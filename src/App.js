330ec3a4bb741115ee85adf28b7c8b6e4e9f741f
Savitoj Cheema <savitojc@Savitojs-MacBook-Air.local>
2021-09-02 21:32:57 +0530

    random word generator implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+import faker from "faker";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Button from "@material-ui/core/Button";
+
+const App = () => {
+  const word = faker.random.word();
+  const [new_word, setNewWord] = React.useState(word);
+  const [words_list, setWordsList] = React.useState([]);
+  const [is_start, setStart] = React.useState(false);
+  const interval = React.useRef({});
+
+  const newWordEffect = () => {
+    const new_list = [...words_list];
+    new_list.push(new_word);
+    setWordsList(new_list);
+  };
+  React.useEffect(newWordEffect, [new_word]);
+
+  const startEffect = () => {
+    if (!is_start) return;
+    interval.current.timer = setInterval(() => {
+      setNewWord(() => faker.random.word());
+    }, 1000);
+  };
+  React.useEffect(startEffect, [is_start]);
+
+  const wordsListEffect = () => {
+    console.log("WORDSLIST", words_list, words_list.length);
+    if (words_list.length >= 10) {
+      clearInterval(interval.current.timer);
+    }
+  };
+  React.useEffect(wordsListEffect, [words_list]);
+
+  console.log("WORDS", words_list);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button
+        onClick={() => {
+          setStart(true);
+        }}
+        variant="contained"
+        color="primary"
+      >
+        Start
+      </Button>
+
+      <List component="nav" aria-label="main mailbox folders">
+        {words_list.map((word) => (
+          <ListItem button>
+            <ListItemText primary={word} />
+          </ListItem>
+        ))}
+      </List>
     </div>
   );
-}
-
+};
 export default App;
